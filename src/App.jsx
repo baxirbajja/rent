6a65e0011fd435f20da7df4aa5db9fb2170083dc
bachirbajja <medbachirbajja@gmail.com>
2025-02-09 01:27:22 +0100

    lastlats

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// had lapp hiya principal app dyalna
 import React from "react";
 import AddAd from "./components/AddAd.jsx";
 import AdsList from "./components/AdsList.jsx";
@@ -10,15 +11,23 @@ import Dashboard from "./components/admin/Dashboard.jsx";
 import PrivateRoute from "./components/PrivateRoute";
 import './App.css';
 
+// hna fin kandirou routing w layout principal
 function App() {
   return (
+    // Router bach n9dro nnt9lo bin les pages
     <Router>
       <div className="App">
+        {/* navbar li gha tb9a f fog */}
         <Navbar />
+        {/* routes dial kol page */}
         <Routes>
+          {/* homepage */}
           <Route path="/" element={<Home />} />
+          {/* page dial login */}
           <Route path="/login" element={<Login />} />
+          {/* page dial inscription */}
           <Route path="/signup" element={<Sign />} />
+          {/* page dial izafat i3lan jdid, khasha login */}
           <Route
             path="/add-ad"
             element={
@@ -27,7 +36,9 @@ function App() {
               </PrivateRoute>
             }
           />
+          {/* page dial lista dial les annonces */}
           <Route path="/ads" element={<AdsList />} />
+          {/* page dial ladmin */}
           <Route
             path="/admin"
             element={
